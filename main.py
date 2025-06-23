27f723559ca9ed87a68ec420d974b88afb976769
littleJname <111864@st-maartenscollege.nl>
2025-06-23 15:13:33 +0200

    Add files via upload
    The geupdated versie van de galgje code met:
    - laten zien welke letters fout zijn op het scherm
    - cijfers en andere tekens worden niet geteld als een fout, maar dat de gebruiker het nog een keer moet doen met een letter

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ def making_a_guess():
     correct_guess = False
     for letter in chosen_word:
         if guess.lower() == chosen_word[x]:
-            blank = ["_"] * len(chosen_word)
+            blank_list[x] = guess.lower()
             correct_guess = True
         x += 1
     if not correct_guess:
-         print(f"Geen {guess}, sorry.")
-         update_display += 1
+        print(f"Geen {guess}, sorry.")
+        incorrect_guess.append(guess.lower())
+        update_display += 1
+
+# functie om te controleren of invoer alleen letters bevat
+def is_valid_letter(input_str):
+    return len(input_str) == 1 and input_str.isalpha()
 
 #hangman tekeningen 
 HANGMAN = (
@@ -111,31 +116,64 @@ HANGMAN = (
 """)
 
 #woordenlijst
-word_list = ["aarde", "informatica", "golrieus", "pauw bidsprinkhaan garnaal", "grass", "skelet", "schaduw", "wolk", "paars", "spongebob", "roblox", "mango", "corny", "pyhton" ]
+woord_lijst = ["aarde", "informatica", "golrieus", "mantis garnaal", "spongebob",  "python", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie",
+"waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk" ]
 
-chosen_word = list(random.choice(word_list))
+chosen_word = list(random.choice(woord_lijst))
 
-blank = "_" * len(chosen_word)
+# fouten antwoorden en het raden van een woord
+blank = ""
+for letter in chosen_word:
+    blank += "_"
+blank_list = list(blank)
 
 update_display = 0
+incorrect_guess = []
 
 # het starten van het spel (introductie)
 
 print(HANGMAN[update_display])
-guess = input(f"je speelt hangman.\n{blank}\nRaad een letter? ")
+while True:
+    if incorrect_guess:
+        guess = input(f"je speelt hangman.\n{blank}\nVerkeerde letters: [{', '.join(incorrect_guess)}]\nRaad een letter? ")
+    else:
+        guess = input(f"je speelt hangman.\n{blank}\nRaad een letter? ")
+    if is_valid_letter(guess):
+        break
+    else:
+        print("Voer alleen een letter in, geen cijfers of andere tekens")
+
 making_a_guess()
 print(HANGMAN[update_display])
-print(''.join(blank))
+print(''.join(blank_list))
+if incorrect_guess:
+    print(f"Verkeerde letters: [{', '.join(incorrect_guess)}]")
+
 
 # Standen (winnen, verliezen, nog een keer raden)
 while update_display < 6:
-    if blank == chosen_word:
+    if blank_list == chosen_word:
         print("Je hebt gewonnen glorieuze KING!")
         break
-    guess = input("Doe het nog een keer ")
+    
+    while True:
+        if incorrect_guess:
+            guess = input(f"Doe het nog een keer\nVerkeerde letters: [{', '.join(incorrect_guess)}]\nRaad een letter: ")
+        else:
+            guess = input("Doe het nog een keer\nRaad een letter: ")
+        if is_valid_letter(guess):
+            break
+        else:
+            print("Voer alleen één letter in, geen cijfers of andere tekens!")
+
+
+# verkeerde letters laten zien
     making_a_guess()
     print(HANGMAN[update_display])
-    print(''.join(blank))
+    print(''.join(blank_list))
+    if incorrect_guess:
+        print(f"Verkeerde letters: [{', '.join(incorrect_guess)}]")
 
 if update_display == 6:
     print("GAME OVER YOU TWAT.")
+    print(f"Het woord was {chosen_word}")
